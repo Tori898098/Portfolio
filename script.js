e7eab78cb117adf6f4e5c513b0e89395c9f1952f
Tori898098 <twebbe2677@gmail.com>
2017-07-19 13:47:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ alert("WELCOME TO MY SPARKLY PAGE!!!");
 function changepicture (){
   document.getElementById('familyphoto').src="me.jpg"
 }
+function extendpage(){
+document.getElementById("demo").innerHTML="Some fun facts about me are that I was born with two extra fingers in which were cut off because they were dead. I was born 3 months early weighing 2lbs 3oz.I have # dogs named zach, Diesel, and Princess. along with a rabbit named Henry. I have 1 sister whos 19, and lives in Washington D.C. My mom is a nurse, and my Dad is in the Military. I run Track, Play Tennis, and Swim. I enjoy hanging out with friends, shopping, and watching movies. My favorite season is Fall.";
+}
